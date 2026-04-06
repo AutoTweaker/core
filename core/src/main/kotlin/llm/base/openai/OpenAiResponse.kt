8e0e06f3f713df1d588ac249bf47dada2acc3b44
白象_elephant <119278153+WhiteElephant-abc@users.noreply.github.com>
2026-04-06 21:50:42 +0800

    移除了OpenAiResponse中的无用导入

--- core/src/main/kotlin/llm/base/openai/OpenAiResponse.kt
+++ core/src/main/kotlin/llm/base/openai/OpenAiResponse.kt
@@ -1,8 +1,5 @@
 package io.github.whiteelephant.autotweaker.core.llm.base.openai
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerialName
-
 abstract class OpenAiResponse {
     abstract val id: String?
     abstract val created: Long?
