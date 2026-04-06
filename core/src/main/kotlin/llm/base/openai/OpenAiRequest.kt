5c509eacc3264b3b4153dfa01578b513a1890e91
白象_elephant <119278153+WhiteElephant-abc@users.noreply.github.com>
2026-04-06 21:32:43 +0800

    重写OpenAiRequest，现在仅包含mimo与deepseek的共有字段

--- core/src/main/kotlin/llm/base/openai/OpenAiRequest.kt
+++ core/src/main/kotlin/llm/base/openai/OpenAiRequest.kt
@@ -1,12 +1,46 @@
 package io.github.whiteelephant.autotweaker.core.llm.base.openai
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
 import io.github.whiteelephant.autotweaker.core.llm.Tool
 
-abstract class OpenAiRequest<Message : OpenAiMessage> {
-    abstract val messages: List<Message>
-    abstract val model: String
+abstract class OpenAiRequest {
+    abstract val model: String?
+    abstract val thinking: Thinking?
+    abstract val frequencyPenalty: Double?
+    abstract val presencePenalty: Double?
+    abstract val responseFormat: ResponseFormat?
+    abstract val stop: List<String>?
+    abstract val stream: Boolean?
     abstract val temperature: Double?
-    abstract val stream: Boolean
-    abstract val tools: List<Tool>?
+    abstract val topP: Double?
+
+    @Serializable
+    data class Thinking(
+        val type: Type
+    ) {
+        @Serializable
+        enum class Type {
+            @SerialName("enabled")
+            ENABLED,
+
+            @SerialName("disabled")
+            DISABLED
+        }
+    }
+
+    @Serializable
+    data class ResponseFormat(
+        val type: Type
+    ) {
+        @Serializable
+        enum class Type {
+            @SerialName("text")
+            TEXT,
+
+            @SerialName("json_object")
+            JSON_OBJECT
+        }
+    }
 }
