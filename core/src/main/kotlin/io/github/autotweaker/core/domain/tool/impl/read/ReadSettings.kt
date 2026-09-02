374a05cc8e3a7b699a92e088d3907b1302e6d244
白象_elephant <WhiteElephant-abc@outlook.com>
2026-09-03 02:49:25 +0800

    更新ReadSettings的ReadFileDescription

--- core/src/main/kotlin/io/github/autotweaker/core/domain/tool/impl/read/ReadSettings.kt
+++ core/src/main/kotlin/io/github/autotweaker/core/domain/tool/impl/read/ReadSettings.kt
@@ -34,7 +34,7 @@ object ReadSettings {
 	
 	@AutoService(SettingDef::class)
 	class ReadFileDescription : StringSetting(
-		"读取一个文件，最大字符数%s，最大行数%s，返回内容的第一行为文件内容的SHA256，第二行开始是文件内容，注意区分。\n请注意：始终使用此工具而不是bash来获取文件内容",
+		"读取一个文件，最大字符数%s，最大行数%s，返回内容的第一行为文件内容的SHA256，第二行开始是文件内容，注意区分。\n请注意：不要使用bash来运行cat，始终使用此工具而不是bash来获取文件内容",
 		zh(
 			"read-file工具的描述"
 		)
